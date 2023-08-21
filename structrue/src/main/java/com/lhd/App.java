caede9c5d0205a7ae998ee5df46a8b6d088d2ef9
wdnmdlqm~ <18146727210@163.com>
2023-08-21 13:51:16 +0800

    我擦

---
+++ structrue/src/main/java/com/lhd/App.java
@@ -0,0 +1,15 @@
+package com.lhd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("sda");
+        System.out.println("ada");
+    }
+}
